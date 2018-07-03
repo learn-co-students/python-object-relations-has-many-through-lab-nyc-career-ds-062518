ccbe9b6c4fa4926ea18516f4c9c8f76fd56a7a7c
mattymecks <mcmecoli@gmail.com>
2018-07-03 17:31:29 -0400

    Done.

--- passenger.py
+++ passenger.py
@@ -1,16 +1,32 @@
-# import Trip class here
+from trip import Trip
 
 class Passenger:
 
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
+   def __init__(self, name, age):
+       self._name = name
+       self._age = age
 
-    def trips(self):
-        pass
+   @property
+   def name(self):
+       return self._name
 
-    def drivers(self):
-        pass
+   @name.setter
+   def name(self, name):
+       self._name = name
 
-    def trip_count(self):
-        pass
+   @property
+   def age(self):
+       return self._age
+
+   @age.setter
+   def age(self, age):
+       self._age = age
+
+   def trips(self):
+       return [trip for trip in Trip._all if trip._passenger  == self]
+
+   def drivers(self):
+       return [trip._driver for trip in Trip._all if trip._passenger == self]
+
+   def trip_count(self):
+       return len(self.trips())
