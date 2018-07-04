c8a70bf8e4b8e3614491b1cadb1df51051ba7a06
Paulina Zheng <paulinaczheng@gmail.com>
2018-07-04 18:02:27 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,16 +1,34 @@
-# remeber to import the Trip class here
-
+from trip import Trip
+# from query import Query
 class Driver:
 
     def __init__(self, name, driving_style):
         self._name = name
         self._driving_style = driving_style
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def driving_style(self):
+        return self._driving_style
+
+    @driving_style.setter
+    def driving_style(self, driving_style):
+        self._driving_style = driving_style
+
     def trips(self):
-        pass
+        return [item for item in Trip.all() if item.driver == self]
+        # return Query.trips(self)
 
     def passengers(self):
-        pass
+        return [item.passenger for item in Trip.all() if item.driver ==self]
 
     def trip_count(self):
-        pass
+        # trip_list = [item for item in Trip.all() if item.driver == self]
+        return len(self.trips())
